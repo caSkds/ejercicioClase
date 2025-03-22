ba6658b159faf3dd618a9dc1ca63b70b0ebddba1
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-03-22 16:39:39 -0600

    Fixed ejercicio 12 to use queue operations

--- Practica 5/Ejercicio12.c
+++ Practica 5/Ejercicio12.c
@@ -1,51 +1,57 @@
-#include "DynamicQueue.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "DynamicQueue.h"
 
 
 
 void main(){
     struct cola *Queue = calloc(1,sizeof(struct cola));
+    struct cola *auxQueue = calloc(1,sizeof(struct cola));
+    int elementos[6];
+
     crearCola(Queue);
+    crearCola(auxQueue);
 
+    int max = 0 ;
+    int aux = 0;
+    for(int i = 0; i < 12; i++){
+        int dato;
+        printf("Ingrese el dato %i:",i+1);
+        scanf("%i",&dato);
+        encolar(Queue,dato);
+    }
+    printf("Primeros elementos de la cola: ");
+    for(int i = 0; i < 12; i++){
+        if (front(Queue) > max){
+            max = front(Queue);
+        }
+        if (i<6){
+            printf("%i ",front(Queue));
+        }
+        encolar(auxQueue,front(Queue));
+        desencolar(Queue);
 
-    
-    int currElement=0;
-    int max = 0;
-    int queueElements[12] ={0};
-    int iterator =0;
-    
-    for(int i = 0; i<12;i++){
-        printf("Ingrese un número \n");
-        scanf("%d",&currElement);
-        encolar(Queue, currElement);
     }
-    
-    struct nodo *verNodo = Queue->frente;
-    while (verNodo != NULL) {
-        queueElements[iterator] = verNodo->dato;
-        iterator++;
-        verNodo = verNodo->siguiente;
+
+    printf("\nElementos invertidos de la cola: ");
+    for(int i = 0; i <6; i++){
+        elementos[i] = front(auxQueue);
+        desencolar(auxQueue); 
     }
-    max = queueElements[0];
-    for(int i =0; i<12; i++){
-        if(queueElements[i]>max){
-            max = queueElements[i];
+    for(int i = 5; i > -1; i--){
+        encolar(auxQueue,elementos[i]);
+    }
+    for(int i = 0; i < 12; i++){
+        if (i>6){
+            printf("%i ",front(auxQueue));
         }
         
-    }
-    printf("El elemento más grande de la cola es: %d\n", max);
-    printf("Los primeros 6 elementos de la cola son:\n");
-    for(int i=0;i<6; i++){
-        printf("%d ", queueElements[i]);
+        desencolar(auxQueue);
         
     }
-    printf("\n");
-    printf("Dichos elementos invertidos son:\n");
-    for(int i=5;i>-1; i--){
-        printf("%d ", queueElements[i]);
-        
-    }
+    printf("\nEl elemento mas grande de la cola es: %d\n", max);
     free(Queue);
-
+    free(auxQueue);
 }
+    
