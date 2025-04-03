c75f4823adadcb06a61db5a22ab2d6c4c965f5a7
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-04-03 12:52:29 -0600

    Added ejercicio 5 and 10

--- Practica 7/Ejercicio5.c
+++ Practica 7/Ejercicio5.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include "ListaSimple.h"
 
-void main(){
-    int deleted;
-    struct lista elements;
-    crearLista(&elements);
-    insertar(&elements,5);
-    insertar(&elements,6);
-    insertar(&elements,7);
-    insertar(&elements,8);
-    insertar(&elements,9);
-    insertar(&elements,10);
-    verLista(&elements);
-    printf("Seleccione el índice a eliminar");
-    do{
-        printf("Por favor elegir un índice del 0 al 5\n");
-        scanf("%d",&deleted);
-    }
-    while(deleted<0||deleted>6);
 
-    borrarIndice(&elements,deleted);
-    verLista(&elements);
-    borrarIndice(&elements,1);
+void main(){
+   int deleted;
+   struct lista elements;
+   crearLista(&elements);
+   insertar(&elements,5);
+   insertar(&elements,6);
+   insertar(&elements,7);
+   insertar(&elements,8);
+   insertar(&elements,9);
+   insertar(&elements,10);
+   verLista(&elements);
+   printf("Seleccione el índice a eliminar\n");
+   do{
+       printf("Por favor elegir un índice del 0 al 5\n");
+       scanf("%d",&deleted);
+   }
+   while(deleted<0||deleted>6);
+
+
+   borrarIndice(&elements,deleted);
+   verLista(&elements);
+  
 }
